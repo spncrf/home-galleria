04720299ad325c6cfd195025da575b8f4a91ae8f
spncrf <31370744+spncrf@users.noreply.github.com>
2024-03-10 17:28:28 -0500

    adding auth redirect workaround

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -20,3 +20,15 @@ export function generateTagData(categories) {
   });
   return categoryData;
 }
+/**
+ * Workaround for auth redirect not working
+ * 
+ * See: https://answers.netlify.com/t/common-issue-netlify-cms-git-gateway-email-not-confirmed/10690/26
+ */
+export function authRedirector() {
+  window.onload(() => {
+    if (window.location.hash.includes('_token=')) {
+      window.location.replace('/admin/' + window.location.hash);
+    }
+  })
+}
